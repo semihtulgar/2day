fc284ad2ceb81182449fb53a6f1d59e57ca23b83
semihtulgar <semihtulgar@gmail.com>
2022-07-28 16:04:06 +0300

    auth

---
+++ ITalent.App/Auth.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITalent.App
+{
+    internal class Auth
+    {
+        public int AuthId { get; set; }
+    }
+}
